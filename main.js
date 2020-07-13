187513c0d15fddc87bf8b7b8a1ec15ef8d6c7f73
Kenneth <kennethkjliang@gmail.com>
2020-07-13 13:12:45 -0700

    added materials costs

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ var funds = 0
 var workforce = 0
 var penCost = 0.25 
 
+var materials = 1000
+var matCost = 10
+
 function sellPen(){
     pens += 1
     funds += penCost 
+    materials -= 1
     document.getElementById("pens").textContent = pens;
     document.getElementById("funds").textContent = funds;
+    document.getElementById("mat").textContent = materials;
 }
 
 function lowerPrice(){
@@ -19,3 +24,13 @@ function raisePrice(){
     penCost += .01
     document.getElementById("penprice").textContent = penCost
 }
+
+function buyMat(){
+    funds -= matCost 
+    document.getElementById("funds").textContent = funds;
+    materials += 200
+    document.getElementById("mat").textContent = materials
+    matCost += 5
+    document.getElementById("matCost").textContent = matCost
+}
+
