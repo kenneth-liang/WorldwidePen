ba9acfd418510b47bca610566d7a31780c4f440c
Kenneth <kennethkjliang@gmail.com>
2020-07-13 14:18:07 -0700

    added basic auto clicker when hiring a person

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var pens = 0 
 var funds = 0 
-var workforce = 0
 var penCost = 0.25 
 
 var materials = 1000
 var matCost = 10
+
+var hireCost = 5 
+var workForce = 0;
 
 function sellPen(){
     pens += 1
@@ -26,11 +28,27 @@ function raisePrice(){
 }
 
 function buyMat(){
+    if (matCost > funds) return
     funds -= matCost 
     document.getElementById("funds").textContent = funds;
     materials += 200
     document.getElementById("mat").textContent = materials
     matCost += 5
     document.getElementById("matCost").textContent = matCost
+}
+
+function hirePerson(){
+    if (hireCost > funds) return 
+    funds -= hireCost
+    document.getElementById("funds").textContent = funds;
+    workForce++
+    document.getElementById("workForce").textContent = workForce;
+    hireCost += 5; 
+    document.getElementById("hireCost").textContent = hireCost;
+
+    //autoclicker 
+    setInterval(function() { 
+        sellPen();
+    },400)
 }
 
