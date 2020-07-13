828fc7294259abee02c91decfde140600232e937
Kenneth <kennethkjliang@gmail.com>
2020-07-13 12:56:42 -0700

    add price change functions and variables

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 var pens = 0 
+var funds = 0 
+var workforce = 0
+var penCost = 0.25 
 
 function sellPen(){
     pens += 1
-    document.getElementById("pens").textContent = "Pens: " + pens;
+    funds += penCost 
+    document.getElementById("pens").textContent = pens;
+    document.getElementById("funds").textContent = funds;
+}
+
+function lowerPrice(){
+    penCost -= .01
+    document.getElementById("penprice").textContent = penCost
+}
+
+function raisePrice(){
+    penCost += .01
+    document.getElementById("penprice").textContent = penCost
 }
