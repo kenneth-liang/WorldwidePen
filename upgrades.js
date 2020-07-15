67763675004b65728b8bee64f59538de1d6544ea
Kenneth <kennethkjliang@gmail.com>
2020-07-14 22:23:36 -0700

    started upgrade1

--- upgrades.js
+++ upgrades.js
@@ -1,2 +1,22 @@
 var upgrades = [] 
 var activeUpgrades = []
+
+var upgrade1 = {
+    id: "upgradeButton1",
+    title: "Enable SalesCorce",
+    priceTag: "$5.00",
+    description: "Increase sales capabilities by 25%",
+    effect: function (){
+        funds -= 5
+        saleRate = .25
+        updateSellRate();
+
+        //upon clicking it remove it from the list
+        //mode code
+    }
+}
+
+
+
+
+
