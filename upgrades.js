5bca282648377b970bc49348d321c08ae18a1371
Kenneth <kennethkjliang@gmail.com>
2020-07-17 21:46:57 -0700

    added additional upgrades

--- upgrades.js
+++ upgrades.js
@@ -7,18 +7,26 @@ let upgrade1 = {
   title: "Company Satchels ",
   priceTag: "$10.00",
   description: "Increase sales capabilities by 25%",
-  message: "Sales employees are equipped with Batagonia Bags to carry more pens",
+  message: "** Employees are equipped with Batagonia Bags to carry more pens",
+  messageAI: "That's awesome! I wish I could wear clothes.",
   cost: function () {
     return funds >= 10;
   },
   setOff: function () {
-    return workForce >= 1;
+    return workForce >= 2;
   },
   uses: 1,
   effect: function () {
     funds -= 10;
     saleRate = Math.ceil(saleRate * 1.25);
     updateSellRate();
+
+    setTimeout(()=> {
+      displayMessage(upgrade1.message)
+       setTimeout(() => {
+         displayMessage(upgrade1.messageAI);
+       }, 2000);
+    },1000);
 
     //upon clicking it remove it from the list
     let ele = document.getElementById("upgradeButton1");
@@ -32,9 +40,10 @@ upgrades.push(upgrade1)
 let upgrade2 = {
   id: "upgradeButton2",
   title: "Enable Scooter Technology ",
-  priceTag: "$15.00",
+  priceTag: "$20.00",
   description: "Increase sales capabilities by 50%",
-  message: "Sales employees mobility increased with scooters",
+  message: "** Mobility increased by riding scooters",
+  messageAI: "Now that is good speed",
   cost: function () {
     return funds >= 20;
   },
@@ -44,9 +53,14 @@ let upgrade2 = {
   uses: 1,
   effect: function () {
     funds -= 20;
-    saleRate = Math.ceil(saleRate * 1.50);
+    saleRate = Math.ceil(saleRate * 1.5);
     updateSellRate();
-
+    setTimeout(() => {
+      displayMessage(upgrade2.message);
+      setTimeout(() => {
+        displayMessage(upgrade2.messageAI);
+      }, 2000);
+    }, 1000);
     //upon clicking it remove it from the list
     let ele = document.getElementById("upgradeButton2");
     ele.parentNode.removeChild(ele);
@@ -61,7 +75,8 @@ let upgrade3 = {
   title: "Company Vehicle ",
   priceTag: "$35.00",
   description: "Increase sales capabilities by 75%",
-  message: "Carrying capacity and mobility increased",
+  message: "** Company Vechicles issued.",
+  messageAI: "Do we have insurance yet?",
   cost: function () {
     return funds >= 35;
   },
@@ -73,7 +88,12 @@ let upgrade3 = {
     funds -= 35;
     saleRate = Math.ceil(saleRate * 1.75);
     updateSellRate();
-
+    setTimeout(() => {
+      displayMessage(upgrade3.message);
+      setTimeout(() => {
+        displayMessage(upgrade3.messageAI);
+      }, 2000);
+    }, 1000);
     //upon clicking it remove it from the list
     let ele = document.getElementById("upgradeButton3");
     ele.parentNode.removeChild(ele);
@@ -88,7 +108,8 @@ let upgrade4 = {
   title: "Research Hyponosis  ",
   priceTag: "$125.00",
   description: "Increase sales capabilities by 100%",
-  message: "Sales employees are now trained to hypnotize customers into buying more pens.",
+  message: "** Sales employees are now trained to hypnotize customers into buying more pens.",
+  messageAI: "Wait, is this legal??",
   cost: function () {
     return funds >= 125;
   },
@@ -100,7 +121,12 @@ let upgrade4 = {
     funds -= 100;
     saleRate = Math.ceil(saleRate * 2.00);
     updateSellRate();
-
+    setTimeout(() => {
+      displayMessage(upgrade4.message);
+      setTimeout(() => {
+        displayMessage(upgrade4.messageAI);
+      }, 2000);
+    }, 1000);
     //upon clicking it remove it from the list
     let ele = document.getElementById("upgradeButton4");
     ele.parentNode.removeChild(ele);
@@ -118,9 +144,9 @@ let upgrade5 = {
   title: "Purchase Manufacturer ",
   priceTag: "$10.00",
   description: "Automatically purchase materials when out",
-  message: "Toggle to enable automatic materials purchasing.",
+  message: "** Toggle to enable automatic materials purchasing.",
   cost: function () {
-    return funds >= 20;
+    return funds >= 10;
   },
   setOff: function () {
     return materials <= 750;
@@ -129,6 +155,7 @@ let upgrade5 = {
   effect: function () {
     funds -= 20;
     document.getElementById("manufacturer").style = "visibility: visible";
+    displayMessage(upgrade5.message);
     let ele = document.getElementById("upgradeButton5");
     ele.parentNode.removeChild(ele);
     let index = activeUpgrades.indexOf(upgrade5);
@@ -142,18 +169,19 @@ let upgrade6 = {
   title: "Bargaining for Dummies ",
   priceTag: "$20.00",
   description: "Increase Materials Purchased by 50%",
-  message: "Purchaed Materials: 1000 -> Purchaed Materials: 1500 ",
+  message: "** Gained more materials at each purchase ",
+  messageAI: "You've gotten pretty good at negotiating.",
   cost: function () {
-    return funds >= 25;
+    return funds >= 20;
   },
   setOff: function () {
-    return matCost >= 12;
+    return pens >= 1000;
   },
   uses: 1,
   effect: function () {
     funds -= 25;
     purchaseMatAmt *= 1.50;
-
+    displayMessage(upgrade6.messageAI);
     let ele = document.getElementById("upgradeButton6");
     ele.parentNode.removeChild(ele);
     let index = activeUpgrades.indexOf(upgrade6);
@@ -165,20 +193,26 @@ upgrades.push(upgrade6);
 let upgrade7 = {
   id: "upgradeButton7",
   title: "Purchase Factory ",
-  priceTag: "$500.00",
+  priceTag: "$200.00",
   description: "Increase Materials Purchased by 180%",
-  message: "Locally create your materials in your own factory. Purchaed Materials: 1500 -> Purchaed Materials: 2700 ",
+  message: "** All materials produced in house factory.",
+  messageAI: "The locals are happy you employed them. You are a good human",
   cost: function () {
-    return funds >= 500;
+    return funds >= 200;
   },
   setOff: function () {
-    return matCost >= 100;
+    return pens >= 1000;
   },
   uses: 1,
   effect: function () {
-    funds -= 1000;
+    funds -= 200;
     purchaseMatAmt *= 1.80;
-
+    setTimeout(() => {
+      displayMessage(upgrade7.message);
+      setTimeout(() => {
+        displayMessage(upgrade7.messageAI);
+      }, 2000);
+    }, 1000);
     let ele = document.getElementById("upgradeButton7");
     ele.parentNode.removeChild(ele);
     let index = activeUpgrades.indexOf(upgrade7);
@@ -190,21 +224,28 @@ upgrades.push(upgrade7);
 let upgrade10 = {
   id: "upgradeButton10",
   title: "Outsource ",
-  priceTag: "$10,000.00",
+  priceTag: "$1,000.00",
   description: "Increase Materials Purchased by 500% and Decrease cost by 50%",
-  message: "All factories relocated over seas. Folks don't seem that happy...",
+  message: "** All factories relocated over seas",
+  messageAI: "Folks don't seem that happy about this...",
   cost: function () {
-    return funds >= 10000;
+    return funds >= 1000;
   },
   setOff: function () {
-    return matCost >= 150;
+    return pens >= 15000;
   },
   uses: 1,
   effect: function () {
-    funds -= 10000;
-    matCost *= .50;
-    purchaseMatAmt *= 5.00;
-    // decrease popularity 
+    funds -= 1000;
+    matCost *= 0.5;
+    purchaseMatAmt *= 5.0;
+    setTimeout(() => {
+      displayMessage(upgrade10.message);
+      setTimeout(() => {
+        displayMessage(upgrade10.messageAI);
+      }, 2000);
+    }, 1000);
+    // decrease popularity
     let ele = document.getElementById("upgradeButton10");
     ele.parentNode.removeChild(ele);
     let index = activeUpgrades.indexOf(upgrade10);
@@ -213,27 +254,51 @@ let upgrade10 = {
 };
 upgrades.push(upgrade10);
 
-
-
-
 let upgrade15 = {
   id: "upgradeButton15",
   title: "Research Artificial Inteligence ",
-  priceTag: "$15,000.00",
+  priceTag: "$5.00",
   description: "If you dare...",
-  message: "AI research beginning...beep...boop...",
   cost: function () {
-    return funds >= 25000;
+    return funds >= 5;
   },
   setOff: function () {
-    return pens >= 500000;
+    return workForce >= 1;
   },
   uses: 1,
   effect: function () {
-    funds -= 25000;
+    funds -= 5;
     researchAI();
-    document.getElementById("aiDiv").style = "visibility: visible";
+    clearMessages();
 
+    // how can i do this better... not dry
+    setTimeout(()=> {
+        displayMessage("Artificial Intelligence research beginning...")
+        setTimeout(()=> {
+          displayMessage("...")
+          setTimeout(() => {
+            displayMessage("beep")
+            setTimeout(() => {
+              displayMessage("...");
+              setTimeout(() => {
+                displayMessage("boop");
+                setTimeout(() => {
+                  displayMessage("...");
+                  setTimeout(() => {
+                    displayMessage("Hello, I am Targis your 'friendly' assistant!");
+                    setTimeout(() => {
+                    document.getElementById("targis").style =
+                      "visibility: visible";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000)
+      }, 1000);
+    
+    
     let ele = document.getElementById("upgradeButton15");
     ele.parentNode.removeChild(ele);
     let index = activeUpgrades.indexOf(upgrade15);
@@ -241,6 +306,40 @@ let upgrade15 = {
   },
 };
 upgrades.push(upgrade15);
+
+
+let upgrade16 = {
+  id: "upgradeButton16",
+  title: "Enhance Targis ",
+  priceTag: "$250.00",
+  description: "Targis learns a new skill ",
+  messageAI: "Beep...Boop... I know statistics",
+  cost: function () {
+    return funds >= 250;
+  },
+  setOff: function () {
+    return pens >= 3000
+  },
+  uses: 1,
+  effect: function () {
+    funds -= 250;
+    targisKnowledge += 10
+    setTimeout(() => {
+      displayMessage(upgrade10.message);
+      setTimeout(() => {
+        displayMessage(upgrade10.messageAI);
+      }, 2000);
+    }, 1000);
+    let ele = document.getElementById("upgradeButton16");
+    ele.parentNode.removeChild(ele);
+    let index = activeUpgrades.indexOf(upgrade15);
+    activeUpgrades.splice(index, 1);
+  },
+};
+upgrades.push(upgrade16);
+
+
+
 
 //more upgrades ALOT 
 
